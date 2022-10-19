f18e2d905e350aa98fa6e6aeae45f02a591273be
Gerson Garayar <garayargerson20@gmail.com>
2022-10-18 20:58:00 -0500

    mejorando mi web

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,30 +12,30 @@ export default function Nav() {
     <>
       <nav
         className={
-        `${active ? "" : "hidden"}
+        `${!active && "hidden"}
         md:h-10 md:block 
         fixed w-full h-full 
         flex justify-center z-40`}
       >
-        <ul className='w-full flex flex-col gap-2 md:flex-row justify-center bg-slate-900/90'>
+        <ul className='w-full flex flex-col gap-2 md:flex-row justify-center bg-neutral-900/90'>
           <li><a onClick={handleClick} className={a} href="#home">Home</a></li>
+          <li><a onClick={handleClick} className={a} href="#projects">Projects</a></li>
           <li><a onClick={handleClick} className={a} href="#about">About</a></li>
           <li><a onClick={handleClick} className={a} href="#skills">Skills</a></li>
-          <li><a onClick={handleClick} className={a} href="#projects">Projects</a></li>
         </ul>
       </nav>
       <button 
-        className="fixed bottom-4 right-4 z-50 w-12 h-12 md:hidden rounded-full bg-slate-700"
+        className="fixed bottom-4 right-4 z-50 w-12 h-12 md:hidden rounded-full bg-neutral-700"
         onClick={handleToggle}
       >
           {
             active
-            ? <i className="fa-solid fa-xmark text-slate-200"></i>
-            : <i className="fa-solid fa-bars text-slate-200"></i>
+            ? <i className="fa-solid fa-xmark text-neutral-200"></i>
+            : <i className="fa-solid fa-bars text-neutral-200"></i>
           }
       </button>
     </>
   )
 }
 
-const a = "block text-lg text-center text-neutral-300 p-2 hover:text-white transition-all text-slate-200"
+const a = "block text-lg text-center text-neutral-300 p-2 hover:text-white transition-all"
