dbb1603ae5f748aba3c916dc7121d2cf0a526f27
Gerson Garayar <garayargerson20@gmail.com>
2022-11-27 21:10:35 -0500

    mejorando portafolio

--- src/components/myProjects.js
+++ src/components/myProjects.js
@@ -1,26 +1,26 @@
 export const myProjects = [
   {
     id: 1,
-    title: "Pokemon",
-    url: "https://pokemon-react-js.netlify.app/",
-    github: "https://github.com/GersonGarayar20/Pokemon",
-    img: "pokemon.png",
-    tag:["react"]
-  },
-  {
-    id: 2,
     title: "Giffy",
     url: "https://giffy-ts.netlify.app/",
     github: "https://github.com/GersonGarayar20/giffy",
-    img: "giffy.png",
+    img: "projects/giffy.png",
     tag:["react", "TypeScript"]
+  },
+  {
+    id: 2,
+    title: "Pokemon",
+    url: "https://pokemon-react-js.netlify.app/",
+    github: "https://github.com/GersonGarayar20/Pokemon",
+    img: "projects/pokemon.png",
+    tag:["react"]
   },
   {
     id: 3,
     title: "My List",
     url: "https://my-list-react.netlify.app/",
     github: "https://github.com/GersonGarayar20/MyList",
-    img: "my-list.png",
+    img: "projects/my-list.png",
     tag:["react"]
   },
   {
@@ -28,7 +28,7 @@ export const myProjects = [
     title: "Agenda",
     url: "https://agenda-react-js.netlify.app/",
     github: "https://github.com/GersonGarayar20/agenda",
-    img: "agenda.png",
+    img: "projects/agenda.png",
     tag:["react"]
   },
   {
@@ -36,7 +36,7 @@ export const myProjects = [
     title: "Login",
     url: "https://login-react-js.netlify.app/",
     github: "https://github.com/GersonGarayar20/login",
-    img: "login.png",
+    img: "projects/login.png",
     tag:["react", "TypeScript"]
   },
 ]
